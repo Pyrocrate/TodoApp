a6ff5a28bdd7de6d54ee87e2afe0b6540856a795
AshminJayson <50941574+AshminJayson@users.noreply.github.com>
2022-02-10 00:15:55 +0530

    Added PagesDirectory

--- todoapp/lib/main.dart
+++ todoapp/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
-import 'todoCards.dart';
+import 'pages/todoCards.dart';
+import 'pages/todo.dart';
 
 void main() => runApp(MaterialApp(
       initialRoute: 'root',
@@ -9,49 +10,3 @@ void main() => runApp(MaterialApp(
         'root': (context) => todo(),
       },
     ));
-
-class todo extends StatefulWidget {
-  const todo({Key? key}) : super(key: key);
-
-  @override
-  _todoState createState() => _todoState();
-}
-
-class _todoState extends State<todo> {
-  List<String> todoitems = ['Turn Off Alarm', 'Brush My Teeth', 'Take a Bath'];
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Center(
-            child: Text(
-              'ToDo App',
-            ),
-          ),
-        ),
-        backgroundColor: Color.fromARGB(255, 149, 221, 255),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: todoitems
-              .map((item) => todoCards(
-                  item: item,
-                  delete: () {
-                    SchedulerBinding.instance
-                        ?.addPostFrameCallback((timeStamp) {
-                      setState(() {
-                        todoitems.remove(item);
-                      });
-                    });
-                  }))
-              .toList(),
-        ),
-        floatingActionButton: FloatingActionButton(
-            onPressed: (() => setState(() {
-                  todoitems.clear();
-                })),
-            child: Icon(Icons.airplane_ticket)),
-      ),
-    );
-  }
-}
