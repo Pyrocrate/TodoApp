465b86ca775a47663654a750bb691cd2176595d5
AshminJayson <50941574+AshminJayson@users.noreply.github.com>
2022-02-17 23:53:34 +0530

    Layout of newtask page

--- todoapp/lib/pages/newtask.dart
+++ todoapp/lib/pages/newtask.dart
@@ -6,19 +6,68 @@ class newTask extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool keyboardIsOpened = MediaQuery.of(context).viewInsets.bottom != 0.0;
     return SafeArea(
       child: Scaffold(
-        body: Column(children: [
-          SizedBox(
-            height: 20,
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: "Enter Task",
-              suffixIcon: Icon(Icons.add),
-            ),
-          )
-        ]),
+        body: SingleChildScrollView(
+          child: Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.red,
+                  height: 250,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                    padding: EdgeInsets.all(30),
+                    child: Wrap(
+                      runSpacing: 20,
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                            // border: OutlineInputBorder(),
+                            labelText: "Task Title",
+                            suffixIcon: Icon(Icons.add),
+                          ),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                            // border: OutlineInputBorder(),
+                            labelText: "Task Subtitle",
+                            suffixIcon: Icon(Icons.add),
+                          ),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                            // border: OutlineInputBorder(),
+                            labelText: "Task Description",
+                            suffixIcon: Icon(Icons.add),
+                          ),
+                          maxLines: 3,
+                        ),
+                      ],
+                    ))
+              ]),
+        ),
+        floatingActionButton: Visibility(
+          visible: !keyboardIsOpened ? true : false,
+          child: ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, 'newTask');
+              },
+              style: ElevatedButton.styleFrom(
+                  fixedSize: Size(150, 50),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30))),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [Icon(Icons.add), Text("Add Task")],
+              )),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       ),
     );
   }
